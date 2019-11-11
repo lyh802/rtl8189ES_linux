e2257ebd855bed49676d0d2750aba55a427c8baa
kimocoder <christian@aircrack-ng.org>
2019-11-11 01:08:21 +0100

    Adds definite sizes for MTUs

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -4716,6 +4716,12 @@ static int rtw_cfg80211_add_monitor_if(_adapter *padapter, char *name,
 		goto out;
 	}
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0))
+	mon_ndev->min_mtu = WLAN_MIN_ETHFRM_LEN;
+	mon_ndev->max_mtu = WLAN_DATA_MAXLEN;
+#endif
+	mon_ndev->mtu = WLAN_DATA_MAXLEN;
+
 	mon_ndev->type = ARPHRD_IEEE80211_RADIOTAP;
 	strncpy(mon_ndev->name, name, IFNAMSIZ);
 	mon_ndev->name[IFNAMSIZ - 1] = 0;
